34029f11b8706cdadfe657e305e295d229740dc1
makarik1 <irinka999@list.ru>
2022-08-07 20:38:04 +0300

    Добавлен тест на наличие кнопки "Добавить в корзину"

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), \
+        "Кнопка добавления товара в корзину отсутсвует"
